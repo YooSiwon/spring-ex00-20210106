83d203e940446e55bbbe7457986e89612bed7017
meari14 <meari14@naver.com>
2021-01-06 15:11:34 +0900

    https://github.com/YooSiwon/spring-ex00-20210106.git

---
+++ src/test/java/org/zerock/sample/SampleTests.java
@@ -0,0 +1,18 @@
+package org.zerock.sample;
+
+import static org.junit.Assert.*;
+
+
+@Log4j
+import org.junit.Test;
+
+public class SampleTests {
+
+	@Test
+	private void testExist() {
+		log.info("-------");
+
+	}
+	
+	
+}
